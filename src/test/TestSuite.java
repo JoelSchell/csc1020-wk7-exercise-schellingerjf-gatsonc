8567f71ad0216bd3758ba00e6f34b6e2dc36443d
Joel Schellinger <schellingerjf@msoe.edu>
2025-10-13 12:01:27 -0500

    Test3

--- src/test/TestSuite.java
+++ src/test/TestSuite.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.*;
 
 public class TestSuite {
     private BankAccount bankAccount;
+    private BankAccount bankAccount1;
 
     @BeforeEach
     public void setup() {
         bankAccount = new BankAccount("Bob", 5000);
+        bankAccount1 = new BankAccount("Billy", 2000);
     }
 
     @Test
@@ -23,6 +25,15 @@ public class TestSuite {
         bankAccount.withdraw(1000);
         Assertions.assertEquals(bankAccount.balanceCents(), 4000);
     }
+
+    @Test
+    @Order(3)
+    public void happyCase3() {
+        bankAccount.transferTo(bankAccount1, 1000);
+        Assertions.assertEquals(bankAccount.balanceCents(), 4000);
+        Assertions.assertEquals(bankAccount1.balanceCents(), 3000);
+    }
+
     @Test
     @Order(4)
     public void boundaryCase1(){

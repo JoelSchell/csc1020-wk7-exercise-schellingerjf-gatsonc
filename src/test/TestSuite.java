47fabf6d7b49d7b167eda035c8251c0b34c0467b
Joel Schellinger <schellingerjf@msoe.edu>
2025-10-13 11:22:16 -0500

    setup method

--- src/test/TestSuite.java
+++ src/test/TestSuite.java
@@ -3,10 +3,13 @@ import org.junit.jupiter.api.*;
 
 
 public class TestSuite {
+    private BankAccount bankAccount;
+
     @BeforeEach
     public void setup() {
-        BankAccount bankAccount;
+        bankAccount = new BankAccount("Bob", 5000);
     }
+
     @Test
     @Order(1)
     public void happyCase1() {

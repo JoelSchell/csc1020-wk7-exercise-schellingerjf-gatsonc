697382b493f6add72f5fb883598f0aec262ca8e6
Joel Schellinger <schellingerjf@msoe.edu>
2025-10-13 20:11:21 -0500

    invalid cases done

--- src/test/TestSuite.java
+++ src/test/TestSuite.java
@@ -36,16 +36,55 @@ public class TestSuite {
 
     @Test
     @Order(4)
-    public void boundaryCase1(){
+    public void boundaryCase1() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> bankAccount.deposit(0));
+    }
 
+    @Test
+    @Order(5)
+    public void boundaryCase2() {
 
     }
+
     @Test
-    @Order(4)
-    public void boundaryCase1(){
-        Assertions.assertThrows(IllegalArgumentException.class, () -> bankAccount.deposit(0));
+    @Order(9)
+    public void invalidCase1() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new BankAccount("", 5000));
+    }
 
+    @Test
+    @Order(10)
+    public void invalidCase2() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new BankAccount("Bob", -1000));
+    }
 
+    @Test
+    @Order(11)
+    public void invalidCase3() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> bankAccount.transferTo(bankAccount1, -1000));
+    }
+
+    @Test
+    @Order(12)
+    public void invalidCase4() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> bankAccount.transferTo(null, 100));
+    }
+
+    @Test
+    @Order(13)
+    public void invalidCase5() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> bankAccount.withdraw(-100));
+    }
+
+    @Test
+    @Order(14)
+    public void invalidCase6() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> bankAccount.deposit(-100));
+    }
+
+    @Test
+    @Order(15)
+    public void invalidCase7() {
+        Assertions.assertThrows(IllegalStateException.class, () -> bankAccount.transferTo(bankAccount1, 10000));
     }
 }
